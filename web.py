9ce836beba50c7228cd5b50093941f4b769cfe16
Avi Wilensky <avi@promediacorp.com>
2015-06-13 23:05:28 -0400

    Fixed 404

--- web.py
+++ web.py
@@ -453,7 +453,7 @@ def is_installed():
 
 @app.errorhandler(404)
 def page_not_found(error):
-  return render_template('blog/404.html', meta_title='404'), 404
+  return render_template('404.html', meta_title='404'), 404
 
 
 @app.template_filter('formatdate')
