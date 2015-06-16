0f6a1483fbf98f19ad02a913d3db6070350d13a3
Avi Wilensky <avi@promediacorp.com>
2015-06-16 11:01:01 -0400

    changed function name

--- web.py
+++ web.py
@@ -456,7 +456,7 @@ def page_not_found(error):
   return render_template('404.html', meta_title='404'), 404
 
 @app.errorhandler(500)
-def page_not_found(error):
+def server_error(error):
   return render_template('404.html', meta_title='500'), 500
 
 @app.template_filter('formatdate')
